a90da52f20cc70c359ed0d20a0f6a89dddd5b489
David Corey Duke <dcoreydukedev@gmail.com>
2020-11-17 22:16:43 -0600

    initial

--- SignUp/wwwroot/js/wizard/modules/TemplateText.tsx
+++
@@ -1,9 +0,0 @@
-﻿export let tempHeader = '<h2>Lorem ipsum dolor sit amet</h2>';
-
-export let tempText = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae nisl in sapien gravida fermentum eu vitae urna. 
-                Suspendisse convallis pharetra lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus in tempus nisl.
-                Curabitur rutrum mauris eu pharetra iaculis.Ut justo quam, tincidunt vitae est quis, lacinia aliquet turpis.Curabitur
-                augue dui, tempor sagittis tempus eget, iaculis id felis.Cras a ex quis urna ornare consectetur.Donec fringilla imperdiet
-                egestas.</p>`;
-
-export let tempInfoText = `<div>${tempHeader}${tempText}</div>`;
