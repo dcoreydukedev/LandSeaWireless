b1593cbd88f4a722de19fa6ff02a4c63df0ad2ee
David Corey Duke <dcoreydukedev@gmail.com>
2020-11-17 22:14:36 -0600

    initial

---
+++ SignUp/wwwroot/js/wizard/modules/ViewModels.tsx
@@ -0,0 +1,43 @@
+﻿///<reference path = "../wizard.tsx" />
+ namespace Wizard {
+
+    interface Address {
+        address: string;
+        city: string;
+        state: string;
+        zip: string;
+    }
+
+    type ClientType = 'person' | 'company';
+
+    interface ContactInfo {
+        firstName: string;
+        lastName: string;
+        email: string;
+        phone: string;
+    }
+
+    interface ServicePlan {
+        servicePlanType: string;
+        name: string;
+        dwnspd: number;
+        uplspd: number;
+        price: number;
+        details: string;
+    }
+
+    interface ServicePlans extends Array<ServicePlan> { }
+
+    interface BillingInfo {
+        firstName: string;
+        lastName: string;
+        address: string;
+        city: string;
+        state: string;
+        zip: string;
+        ccn: string;
+        ccv: string;
+        cce: string;
+    }
+}
+
---
+++ SignUp/wwwroot/js/wizard/wizard.tsx
@@ -0,0 +1,31 @@
+﻿import { RequiredField, RequiredFieldProps } from './modules/RequiredField';
+import *  as RegexValidation from './modules/RegexValidation';
+import * as templateText from './modules/TemplateText';
+import * as steps from './modules/Steps';
+
+namespace Wizard {
+
+    // #region Data Binding
+    export function DataBind(element: { value: any; addEventListener: (arg0: string, arg1: any, arg2: boolean) => void; }, data: any) {
+        this.data = data;
+        this.element = element;
+        element.value = data;
+        element.addEventListener("change", this, false);
+    }
+
+    DataBind.prototype.handleEvent = function (event: { type: any; }) {
+        switch (event.type) {
+            case "change": this.change(this.element.value);
+        }
+    };
+
+    DataBind.prototype.change = function (value: any) {
+        this.data = value;
+        this.element.value = value;
+    };
+
+    // #endregion
+
+    
+
+}
